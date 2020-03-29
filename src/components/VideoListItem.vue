391aee0f29eda5b404c1367ca34a43504236e55b
katawut.io <katawutu.io@gmail.com>
2020-03-29 22:07:05 +0700

    setup style in video list

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,6 +1,6 @@
 <template>
-  <li class="list-group-item">
-    <img :src="thumbnailUrl" />
+  <li class="list-group-item d-flex">
+    <img class="mr-3" :src="thumbnailUrl" />
     {{ video.snippet.title }}
   </li>
 </template>
@@ -17,4 +17,11 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+li {
+  cursor: pointer;
+}
+li:hover {
+  background-color: #eee;
+}
+</style>
