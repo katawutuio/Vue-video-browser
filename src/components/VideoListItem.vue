97bd9f8f1034bbb66f744e1b4a1837795df23ae5
katawut.io <katawutu.io@gmail.com>
2020-03-29 21:49:18 +0700

    set thumbnail image

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,11 +1,19 @@
 <template>
-  <li>{{ video.snippet.title }}</li>
+  <li class="list-group-item">
+    <img :src="thumbnailUrl" />
+    {{ video.snippet.title }}
+  </li>
 </template>
 
 <script>
 export default {
   name: 'VideoListItem',
-  props: ['video']
+  props: ['video'],
+  computed: {
+    thumbnailUrl: function() {
+      return this.video.snippet.thumbnails.default.url;
+    }
+  }
 };
 </script>
 
